15f7758c7d0a67fdc7f216c75b94c409c7496368
Nicolas Wagner <contact@wagner-nicolas.com>
2020-02-02 18:17:13 +0100

    feat: upload pdf to arxweave and add get single document

--- arxweave_API.js
+++ arxweave_API.js
@@ -2,6 +2,7 @@ const express = require('express')
 const AWS = require('aws-sdk')
 const axios = require('axios')
 const parser = require('fast-xml-parser')
+const pdf2base64 = require('pdf-to-base64')
 
 const ArweaveService = require('./arweave.js')()
 
@@ -18,10 +19,37 @@ AWS.config.update({
 
 const dynamoDB = new AWS.DynamoDB()
 
+// Get all arXiv documents
 app.get('/', (request, response) => {
   // TODO: max items to get
-  // returns all items
-  res.send('Hello world  ! ')
+  // returns all items with pagination
+  res.send('Hello world !')
+})
+
+// Get single arXiv document
+app.get('/arxiv/:arXivID', (request, response) => {
+  const arXivID = request.params.arXivID
+
+  const params = {
+    TableName: 'Arxweave',
+    Key: {
+      'arXivID': {S: arXivID}
+    }
+  }
+
+  dynamoDB.getItem(params, async (err, data) => {
+    if (err)
+      console.log('Error', err)
+    else {
+      // If object does not exists, add it in dynamoDB and Arweave.
+      if(Object.keys(data).length === 0) {
+        response.send({
+          status: 'Bad Request',
+          msg: `This arXiv entry does not exist in Arxweave.`}
+        )
+      } else response.send(data)
+    }
+  })
 })
 
 app.post('/new', async (request, response) => {
@@ -34,40 +62,61 @@ app.post('/new', async (request, response) => {
     }
   }
 
-  dynamoDB.getItem(params, (err, data) => {
+  dynamoDB.getItem(params, async (err, data) => {
     if (err)
-      console.log("Error", err)
+      console.log('Error', err)
     else {
       // If object does not exists, add it in dynamoDB and Arweave.
       if(Object.keys(data).length === 0) {
-        axios.get(`https://export.arxiv.org/api/query?id_list=${arXivID}`)
-          .then(responseArxiv => parser.convertToJson(parser.getTraversalObj(responseArxiv.data, {}), {}).feed.entry)
-          .then(entry => dynamoDB.putItem({
-            TableName: 'Arxweave',
-            Item: {
-              'arXivID' : {S: arXivID},
-              'arXivURL' : {S: entry.id},
-              'authors' : {S: JSON.stringify(entry.author)},
-              'updated' : {S: entry.updated},
-              'published' : {S: entry.published},
-              'title' : {S: entry.title},
-              'summary' : {S: entry.summary},
-              'pdf_link' : {S: entry.id.replace("abs", "pdf")}
-            }
-          }, (err, data) => {
-            if (err)
-              console.log("Error", err)
-            else
-              console.log("Success", data)
-          }))
-          .then(() => ArweaveService.createDataTx({
-            data: `<html>${arXivID}</html>`, // TODO: put blob arXiv PDF.
-            tags: [{ 'Content-Type': 'text/html' }] // TODO: put arXiv metadata.
-          }))
-          .then(rawTx => ArweaveService.broadcastTx({ tx: rawTx }))
-          .then(broadcastedTx => response.send({msg: `Data is uploading to arweave with this broadcast ID ${broadcastedTx.id}.`}))
-          .catch(console.log)
-      } else response.send({msg: `This arXiv entry is already uploaded.`})
+        try {
+          const responseArxiv = await axios.get(`https://export.arxiv.org/api/query?id_list=${arXivID}`)
+          const entry = parser.convertToJson(parser.getTraversalObj(responseArxiv.data, {}), {}).feed.entry
+
+          const arXivPdfBase64 = await pdf2base64(entry.id.replace('abs', 'pdf'))
+
+          const arweaveTxPrice = await axios.get(`https://arweave.net/price/${Buffer.byteLength(arXivPdfBase64, 'utf8')}`)
+
+          const rawTx = await ArweaveService.createDataTx({
+            data: arXivPdfBase64,
+            reward: `${arweaveTxPrice.data}`,
+            tags: [{ 'Encode': 'base64', 'Content-Type': 'application/pdf' }] // TODO: put arXiv metadata.
+          })
+
+          const broadcastedTx = await ArweaveService.broadcastTx({ tx: rawTx })
+
+          if (broadcastedTx.id)
+            await dynamoDB.putItem({
+              TableName: 'Arxweave',
+              Item: {
+                'arXivID': {S: arXivID},
+                'arXivURL': {S: entry.id},
+                'authors': {S: JSON.stringify(entry.author)},
+                'updated': {S: entry.updated},
+                'published': {S: entry.published},
+                'title': {S: entry.title},
+                'summary': {S: entry.summary},
+                'pdfLink': {S: entry.id.replace("abs", "pdf")},
+                'broadcastedTxID': {S: broadcastedTx.id},
+                'status': {S: `${broadcastedTx.status}`}
+              }
+            }, (err, data) => {
+              if (err)
+                console.log('Error', err)
+              else
+                console.log('Success', data)
+            })
+
+          response.send({
+            status: broadcastedTx.status === 200 ? 'Success' : 'Error',
+            msg: `Data is uploading to arweave with this broadcast ID ${broadcastedTx.id} and Arweave status ${broadcastedTx.status}.`
+          })
+        } catch (error) {
+          console.error(error)
+        }
+      } else response.send({
+        status: 'Bad Request',
+        msg: `This arXiv entry is already uploaded.`}
+      )
     }
   })
 })
